ec75a49286f7129b155c9b2fa828fd5b63830913
mothtoaflame44 <listetriafusion@gmail.com>
2025-10-06 01:57:20 +0600

    customer panel & major pages mastered

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,11 +8,76 @@ class HomeController extends Controller
 {
    public function index()
    {
-    return view('index');
+    return view('frontend.index');
    }
 
-   public function contact()
+   public function shop()
    {
-    return view('contact');
+    return view('frontend.shop');
    }
+
+   public function returnProcess()
+   {
+    return view('frontend.return-process');
+   }
+  public function viewCart()
+   {
+    return view('frontend.view-cart');
+   }
+
+  public function checkOut()
+   {
+    return view('frontend.checkout');
+   }
+
+  public function categoryProduct()
+   {
+    return view('frontend.category-product');
+   }
+
+  public function subCategoryProduct()
+  {
+   return view('frontend.sub-category-product');
+  }
+  public function details()
+   {
+    return view('frontend.details');
+
+   }
+  public function typeProducts()
+  {
+    return view('frontend.type-products');
+   }
+
+
+  public function privacyPolicy()
+ {
+  return view('frontend.privacy-policy');
+ }
+
+ public function termsCondition()
+ {
+  return view('frontend.terms-condition');
+ }
+
+ public function refundPolicy()
+ {
+  return view('frontend.refund-policy');
+ }
+public function paymentPolicy()
+ {
+  return view('frontend.payment-policy');
+ }
+public function aboutUs()
+{
+ return view('frontend.aboutus');
+}
+
+public function contactUs()
+{
+ return view('frontend.contact-us');
+}
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,43 @@ use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
-Route::get('/contact-us',[HomeController::class, 'contact']);
+Route::get('/shop', [HomeController::class, 'shop']);
+Route::get('/return-process', [HomeController::class, 'returnProcess']);
+Route::get('/view-cart', [HomeController::class, 'viewCart']);
+Route::get('/checkout', [HomeController::class, 'checkOut']);
+Route::get('/category-product', [HomeController::class, 'categoryProduct']);
+Route::get('/sub-category-product', [HomeController::class, 'subCategoryProduct']);
+Route::get('/details', [HomeController::class, 'details']);
+Route::get('/type-products', [HomeController::class, 'typeProducts']);
+Route::get('/privacy-policy', [HomeController::class, 'privacyPolicy']);
+Route::get('/terms-condition', [HomeController::class, 'termsCondition']);
+Route::get('/refund-policy', [HomeController::class, 'refundPolicy']);
+Route::get('/payment-policy', [HomeController::class, 'paymentPolicy']);
+Route::get('/aboutus', [HomeController::class, 'aboutUs']);
+Route::get('/contact-us', [HomeController::class, 'contactUs']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
